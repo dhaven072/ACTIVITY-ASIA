c55e1d470b610a11229d561757e2b9edec6e5079
dhaven072 <22-32041@g.batstate-u.edu.ph>
2025-05-04 22:17:34 +0800

    dhaven072

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Plata, Dhaven Jun ASIA-ACTIVITY</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
